0522e7a02fa84679aaf2c693c46a33a913618af0
lumberman88 <spotifyuser998@gmail.com>
2022-10-21 21:20:54 +0800

    Gen both child nodes in runnder function to ensure at least one or more are operator nodes. Ensures max_depth is reached.

--- with_multiplication.py
+++ with_multiplication.py
@@ -12,16 +12,20 @@ class Node:
 
 
 def gen_subtree(depth, max_depth):
-    if depth == max_depth:
-        return None
-    is_operator = random.randint(0, 1) and depth < max_depth - 1
-    if is_operator:
-        parent = Node(operators[random.randint(0, len(operators)) - 1])
-        parent.left = gen_subtree(depth + 1, max_depth)
-        parent.right = gen_subtree(depth + 1, max_depth)
-    else:
-        parent = Node(random.randint(1, 9))
-    return parent
+    if depth == max_depth - 1:
+        return Node(random.randint(1, 9)), Node(random.randint(1, 9))
+    # either 1 or both are operators
+    children = []
+    at_least_1_operator = False
+    for i in range(2):
+        is_operator = random.randint(0, 1) or (i == 1 and not at_least_1_operator)
+        if is_operator:
+            at_least_1_operator = True
+            children.append(Node(operators[random.randint(0, len(operators)) - 1]))
+            children[-1].left, children[-1].right = gen_subtree(depth + 1, max_depth)
+        else:
+            children.append(Node(random.randint(1, 9)))
+    return children[0], children[1]
 
 
 def apply_value(sum, number, operator):
@@ -57,10 +61,9 @@ def tree_to_str(root):
 if __name__ == '__main__':
     random.seed(time.time())
     root = Node(operators[random.randint(0, len(operators)) - 1])
-    max_depth = random.randint(2, 4)
+    max_depth = random.randint(5, 5)
     # recursive to gen children for parent (root in this case)
-    root.left = gen_subtree(0, max_depth)
-    root.right = gen_subtree(0, max_depth)
+    root.left, root.right = gen_subtree(0, max_depth)
     # form a string
     sum_value = tree_to_str(root)
     print('Str:', universal_str)
